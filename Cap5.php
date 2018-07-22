485dc9dfa94c0bbf63cd6b2daee4e0e7027b2ea1
FelixFelir <41408108+FelixFelir@users.noreply.github.com>
2018-07-22 16:12:46 -0300

    Update Cap5.php

--- Cap5.php
+++ Cap5.php
@@ -9,14 +9,6 @@
 	$nombre="felipe";
 	echo gettype($nombre);
 
-
-echo "# CursoPHP" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git remote add origin https://github.com/FelixFelir/CursoPHP.git
-git push -u origin master
-
 ?>
 </body>
 </html>
