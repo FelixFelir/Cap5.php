6d263b1c2fcae54d46c101f6972310c68b49e093
FelixFelir <41408108+FelixFelir@users.noreply.github.com>
2018-07-22 16:12:25 -0300

    Add files via upload

---
+++ Cap5.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Cap5</title>
+</head>
+<body>
+<?php
+	
+	$nombre="felipe";
+	echo gettype($nombre);
+
+
+echo "# CursoPHP" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/FelixFelir/CursoPHP.git
+git push -u origin master
+
+?>
+</body>
+</html>
